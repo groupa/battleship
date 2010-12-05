3ce03e267a2f2e00a12be177976e94b754c0db53
Kirill Blazhko <kiruri@gmail.com>
2010-12-05 15:01:23 -0200

    Added some comments

--- ComputerShot.java
+++ ComputerShot.java
@@ -1,11 +1,16 @@
 class ComputerShot {
 
+	/**Define counter variables for the 'for' loops: */
 	int i,j;
 
 
+
 	void shotCheckerSteps(Map enemyMap) {
+		/**Take each row of a 2d array: */
 		for (i=0; i < enemyMap.GetMap().length; i++) {
+			/**Take each 2nd element of this row (a checkers map): */
 			for (j=0; j < enemyMap.GetMap()[i].length; j = j + 2) {
+
 				enemyMap.setCell(i,j,1);
 
 
