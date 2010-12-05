12d727efe5fb549f271b1f0fb84e9d7723871afd
Kirill Blazhko <kiruri@gmail.com>
2010-12-05 14:59:24 -0200

    Generate a checkersMap and assign a value to each cell works. The full map at the end of the method is shown

--- ComputerShot.java
+++ ComputerShot.java
@@ -1,24 +1,22 @@
-
-
-
-
 class ComputerShot {
 
 	int i,j;
 
 
+	void shotCheckerSteps(Map enemyMap) {
+		for (i=0; i < enemyMap.GetMap().length; i++) {
+			for (j=0; j < enemyMap.GetMap()[i].length; j = j + 2) {
+				enemyMap.setCell(i,j,1);
 
 
+			}
 
-	void generateAllShotLocations (Map enemyMap)
-	{
+		}
 		for (i=0; i < enemyMap.GetMap().length; i++) {
 			for (j=0; j < enemyMap.GetMap()[i].length; j++) {
-				System.out.println("Hi: " + enemyMap.GetMap()[i][j]);
+System.out.print(" " + enemyMap.getCell(i,j));
 			}
+		System.out.println("");
 		}
-
-
 	}
-
 }
--- Main.java
+++ Main.java
@@ -6,6 +6,6 @@ public static void main (String[] args) {
 
 	Map enemyMap = new Map(10,10);
 	ComputerShot computerShot = new ComputerShot ();
-	computerShot.generateAllShotLocations(enemyMap);
+	computerShot.shotCheckerSteps(enemyMap);
 }
 }
