7d5ae43919a296d9c4808820a59ab9683a50174e
Dmitry Igoshin <dmitry@digoshin.ru>
2010-12-10 23:26:04 +0100

    beta2

---
+++ beta2/Shot.java
@@ -0,0 +1,36 @@
+
+/**
+ *
+ * @author dmitryigoshin
+ */
+public class Shot {
+    int X;
+    int Y;
+
+    Shot(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    int getX ()
+    {
+	    return X;
+    }
+
+    int getY ()
+    {
+	    return Y;
+    }
+
+    void setX (int x)
+    {
+	    X = x;
+    }
+
+    void setY (int y)
+    {
+    	Y = y;
+    }
+
+}
